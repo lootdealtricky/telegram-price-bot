22927baa6a4437dfe3fdfa0816686b533ecbcc62
lootdealtricky <lootdealtricky@gmail.com>
2025-12-23 20:02:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
+module.exports = {
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  CHANNEL_ID: process.env.CHANNEL_ID,
+  TRIGGER_KEYWORDS: ["deal","loot"],
+  CHECK_INTERVAL_MINUTES: 5,
+  PRICE_INCREASE_PERCENT: 20,
+  CHANNEL_NAME_TEXT: "@YourChannel",
+  BOT_TAG: "@lootdealtrickybot"
+};
 
