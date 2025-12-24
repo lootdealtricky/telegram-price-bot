c45726f16e164a052f1d212b328a134586059e1a
lootdealtricky <lootdealtricky@gmail.com>
2025-12-24 11:14:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  BOT_TOKEN: "8479520879:AAE6ud0daLkUtUAOo5n5PhTFMFkQ1EuWkPA",
+  CHANNEL_ID: "-1001186490417", // @lootdealtricky या numeric id
+  TRIGGER_KEYWORDS: ["deal", "loot", "offer"],
+  CHECK_INTERVAL_MINUTES: 5,
+  PRICE_INCREASE_PERCENT: 20,
+  CHANNEL_NAME_TEXT: "@Lootdealtricky",
+  BOT_TAG: "@Ldt_admin_bot"
+};
