e86ec3ac28032bfc3d2b89aae7df64fb7062918e
lootdealtricky <lootdealtricky@gmail.com>
2025-12-27 11:08:30 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,5 +3,9 @@
 set -o errexit
 
 npm install
-# Chrome ki zaroori cheezein install karne ke liye
+
+# Chrome install karne ke liye commands
+apt-get update && apt-get install -y wget gnupg
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 apt-get update && apt-get install -y google-chrome-stable
