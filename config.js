02e3d373f8e1948e091c7b8fbad2f330274f6b9a
lootdealtricky <lootdealtricky@gmail.com>
2025-12-27 09:20:21 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  BOT_TOKEN: process.env.BOT_TOKEN,
-  CHANNEL_ID: process.env.CHANNEL_ID,
-  TRIGGER_KEYWORDS: ["deal", "loot"],
-  CHECK_INTERVAL_MINUTES: 5,
-  PRICE_INCREASE_PERCENT: 20,
-  CHANNEL_NAME_TEXT: "@Lootdealtricky",
-  BOT_TAG: "@lootdealtrickybot"
-};
